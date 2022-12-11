680ae2608cb865ec908b9bbb5bd89442cd438159
Lyudmila Maximenko <109598383+LyudmilaMax@users.noreply.github.com>
2022-12-12 00:57:47 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+		return
+	}
+
+    const selectedImage = e.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
+
+    instance.show()
+    
+    gallery.addEventListener('keydown', e => {
+		if (e.key === 'Escape') {
+			instance.close()
+		}
+	})
+})
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+         </a>
+    </div>`
+    )
+    .join("");
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG')
+        return;
+    
+    let galleryLightbox = new SimpleLightbox('.gallery a', {
+    captionData: "alt",
+	captionDelay: 250,
+})
+    });
+
+
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+         </a>
+    </div>`
+    )
+    .join("");
+}
+
